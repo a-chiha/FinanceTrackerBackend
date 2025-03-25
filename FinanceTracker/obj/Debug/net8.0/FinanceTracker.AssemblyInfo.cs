549c429cfdf866f66c5ee723cf39517527a50667
andr522g <166645177+andr522g@users.noreply.github.com>
2025-03-25 14:15:35 +0100

    Test

--- FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
+++ FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14935f2e02e7e1fc1c5a6dc9b7a0adfa64395aba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d6f5c14a02fd703e0757af71761c8ec7c5e5ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
