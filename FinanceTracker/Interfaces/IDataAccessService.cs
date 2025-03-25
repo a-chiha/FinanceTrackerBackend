07289c283d969502a3d0b21979382307b06e1b2d
andr522g <166645177+andr522g@users.noreply.github.com>
2025-03-25 14:28:05 +0100

    Added Interface

---
+++ FinanceTracker/Interfaces/IDataAccessService.cs
@@ -0,0 +1,11 @@
+﻿namespace FinanceTracker.Interfaces
+{
+    public interface IDataAccessService<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
--- FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
+++ FinanceTracker/obj/Debug/net8.0/FinanceTracker.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d6f5c14a02fd703e0757af71761c8ec7c5e5ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ba23232a73c4f50d421455b5602d4030401a52")]
 [assembly: System.Reflection.AssemblyProductAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FinanceTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
