bec11cadda59f817a74c2dd0a55c14930a785ee2
Ali Najafi <202307397@post.au.dk>
2025-03-25 15:44:13 +0100

    Done with implementation of generic Dataaccesservice and interface

---
+++ FinanceTracker/DataAccess/IDataAccessService.cs
@@ -0,0 +1,8 @@
+﻿public interface IDataAccessService<T> where T : class
+{
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(params object[] keyValues);
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
