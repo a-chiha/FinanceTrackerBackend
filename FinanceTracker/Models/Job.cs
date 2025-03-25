e0853658fa02fb5fc7a97fd83dd4a579b2ff3898
ahmadchiha01@gmail.com <ahmadchiha01@gmail.com>
2025-03-25 14:14:45 +0100

    Relation mellem entities

--- FinanceTracker/Models/Job.cs
+++ FinanceTracker/Models/Job.cs
@@ -1,12 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 
 namespace FinanceTracker.Models;
 
+[PrimaryKey(nameof(UserId), nameof(CVR))]
 public class Job
 {
     public string TaxCard { get; set; }
     public string EmploymentType { get; set; }
-    [Key]
     public int CVR { get; set; }
     public decimal HourlyRate { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; }
 }
--- FinanceTracker/Models/User.cs
+++ FinanceTracker/Models/User.cs
@@ -5,5 +5,5 @@ public class User
     public int UserId { get; set; }
     public string Name { get; set; }
     public string PhoneNumber { get; set; }
-
+    public ICollection<Job> Job { get; set; }
 }
