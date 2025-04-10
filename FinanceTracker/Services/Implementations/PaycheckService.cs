a6cb8760f725d5981929be766c8f2861ac3c7f72
andr522g <166645177+andr522g@users.noreply.github.com>
2025-04-10 22:10:31 +0200

    Små Muhandificeringer

--- FinanceTracker/Services/Implementations/PaycheckService.cs
+++ FinanceTracker/Services/Implementations/PaycheckService.cs
@@ -1,20 +1,25 @@
 ﻿using FinanceTracker.DataAccess;
 using FinanceTracker.Models;
+using FinanceTracker.Services.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
-namespace FinanceTracker.Services
+namespace FinanceTracker.Services.Implementations
 {
-    public class PaycheckService
+    public class PaycheckService : IPaycheckService
     {
         private readonly FinanceTrackerContext _context;
+        private readonly IPaycheckValidation _validation;
 
-        public PaycheckService(FinanceTrackerContext context)
+        public PaycheckService(FinanceTrackerContext context, IPaycheckValidation validation)
         {
             _context = context;
+            _validation = validation;
         }
 
         public async Task<decimal> CalculateSalaryAfterTax(Job job)
         {
+            _validation.ValidateJob(job);
+
             decimal salaryBeforeTax = await CalculateSalaryBeforeTax(job);
             decimal taxDeduction = await CalculateTaxDeduction(job);
             decimal salaryAfterTax = salaryBeforeTax - taxDeduction;
@@ -23,14 +28,19 @@ namespace FinanceTracker.Services
 
         public async Task<decimal> CalculateTaxDeduction(Job job)
         {
+            _validation.ValidateJob(job);
+
             decimal salaryBeforeTax = await CalculateSalaryBeforeTax(job);
             decimal taxRate = job.PaycheckInfo.Tax;
             decimal taxDeduction = salaryBeforeTax * taxRate;
+
             return taxDeduction;
         }
 
         public async Task<decimal> CalculateSalaryBeforeTax(Job job)
         {
+            _validation.ValidateJob(job);
+
             TimeSpan workedTime = await CalculateWorkedHours(job.PaycheckInfo);
             decimal totalHours = (decimal)workedTime.TotalHours;
 
@@ -39,17 +49,26 @@ namespace FinanceTracker.Services
 
         public async Task<TimeSpan> CalculateWorkedHours(PaycheckInfo paycheckInfo)
         {
+            _validation.ValidatePaycheckInfo(paycheckInfo);
+
             var workShifts = await GetWorkShiftsAsync(paycheckInfo.Id);
 
             TimeSpan totalWorkedHours = TimeSpan.Zero;
             foreach (var workShift in workShifts)
             {
-                totalWorkedHours += workShift.EndTime - workShift.StartTime;
+                try
+                {
+                    _validation.ValidateWorkShift(workShift);
+                    totalWorkedHours += workShift.EndTime - workShift.StartTime;
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine($"Habibi der noget galt med dine workshifts");
+                }
             }
 
             return totalWorkedHours;
-        }
-        
+        }        
 
         public async Task<List<WorkShift>> GetWorkShiftsAsync(int paycheckInfoId) // Tilføj validering af datoer
         {
