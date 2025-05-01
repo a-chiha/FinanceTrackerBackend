7e74b80aa6e5ba47d84b078a4a7466e8e3c8d6bd
Ali Najafi <202307397@post.au.dk>
2025-05-01 14:52:41 +0200

    removed redundant api endpoints and renamed enpoints

--- FinanceTracker/Controllers/PaycheckController.cs
+++ FinanceTracker/Controllers/PaycheckController.cs
@@ -31,7 +31,7 @@ namespace FinanceTracker.Controllers
             _job = job;
         }
 
-        [HttpPost("RegisterWorkshift")]
+        [HttpPost("workshift")]
         [Authorize]
         [ResponseCache(CacheProfileName = "NoCache")]
         public async Task<IActionResult> RegisterWorkShift(WorkShiftDTO workShift)
@@ -56,7 +56,19 @@ namespace FinanceTracker.Controllers
 
         }
 
-        [HttpGet]
+        [HttpGet("workshifts")]
+        [Authorize]
+        public async Task<IActionResult> GetAllWorkShiftsForUser()
+        {
+            var UserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var workshifts = await _workShift.GetFilteredAsync(x => x.UserId == UserId);
+
+            return Ok(workshifts);
+        }
+
+
+        [HttpGet("paycheck")]
         [Authorize]
         [ResponseCache(CacheProfileName = "NoCache")]
         public async Task<IActionResult> GeneratePayCheckForMonth(string companyName, int month)
@@ -97,70 +109,7 @@ namespace FinanceTracker.Controllers
             return Ok(paycheck);
         }
 
-        [HttpGet("jobs")]
-        [Authorize]
-        [ResponseCache(CacheProfileName = "NoCache")]
-        public async Task<IActionResult> GetAllUserJobs()
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            
-            if (string.IsNullOrEmpty(userId))
-            {
-                return Unauthorized("User not authenticated");
-            }
 
-            var allJobs = await _job.GetFilteredAsync(j => j.UserId == userId);
-            
-            return Ok(allJobs);
-        }
 
-        [HttpGet("job/{companyName}/month/{month}")]
-        [Authorize]
-        [ResponseCache(CacheProfileName = "NoCache")]
-        public async Task<IActionResult> GeneratePaycheckForSpecificJob(string companyName, int month)
-        {
-            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-            
-            if (string.IsNullOrEmpty(userId))
-            {
-                return Unauthorized("User not authenticated");
-            }
-
-            var job = await _job.GetByIdAsync(userId, companyName);
-            
-            if (job == null)
-            {
-                return NotFound($"Job at company '{companyName}' not found");
-            }
-
-            var workShifts = await _workShift.GetFilteredAsync(w => 
-                w.UserId == userId && 
-                w.StartTime.Month == month);
-            
-            TimeSpan totalWorkedHours = TimeSpan.Zero;
-            foreach (var workShift in workShifts)
-            {
-                totalWorkedHours += workShift.EndTime - workShift.StartTime;
-            }
-            
-            decimal baseSalary = (decimal)totalWorkedHours.TotalHours * job.HourlyRate;
-            decimal amcontribution = baseSalary * 0.08m;
-            decimal salaryAfterAM = baseSalary - amcontribution;
-            decimal tax = 0.37m;
-            decimal taxDeduction = salaryAfterAM * tax;
-            decimal salaryAfterTax = salaryAfterAM - taxDeduction;
-
-            var paycheck = new Paycheck()
-            {
-                SalarayBeforeTax = baseSalary,
-                WorkedHours = totalWorkedHours,
-                AMContribution = amcontribution,
-                Tax = tax,
-                SalarayAfterTax = salaryAfterTax,
-                taxDeduction = taxDeduction
-            };
-
-            return Ok(paycheck);
-        }
     }
 }
